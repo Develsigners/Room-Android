1cf49a784f4201eca02881023ed8ac01c88701dc
gsrathoreniks <niks.gajsa@gmail.com>
2020-10-28 22:44:45 +0530

    Database class created

---
+++ app/src/main/java/in/gsrathoreniks/room/data/UserDatabase.kt
@@ -0,0 +1,46 @@
+package `in`.gsrathoreniks.room.data
+
+import android.content.Context
+import android.service.autofill.UserData
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase : RoomDatabase(){
+
+//    Make UserDatabase a SINGLETON class as we want only a SINGLE instance of UserDatabase running
+
+
+//    this function will return the UserDao which we created
+    abstract fun userDao(): UserDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getDatabase(context: Context): UserDatabase{
+            val tempInstance = INSTANCE
+
+//            check if there is any existing instance is present for our room database
+//            if there exist an existing instance then we'll return that instance
+            if(tempInstance!=null){
+                return tempInstance
+            }
+
+//            If there is no any instance present for our database then we'll create a new instance
+//            WHY SYNCHRONIZED ?? --> Because everything inside the synchronized block will be protected
+//            by concurrent execution on multiple threads
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                            UserDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+
+        }
+    }
+}
