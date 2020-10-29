ad6521e6277880537e5024b2c0f95b8d295cab5c
gsrathoreniks <niks.gajsa@gmail.com>
2020-10-29 20:16:01 +0530

    Getting data from ROOM DB

---
+++ app/src/main/java/in/gsrathoreniks/room/fragments/list/ListAdapter.kt
@@ -0,0 +1,39 @@
+package `in`.gsrathoreniks.room.fragments.list
+
+import `in`.gsrathoreniks.room.R
+import `in`.gsrathoreniks.room.data.User
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.row_item.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_item,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+       val currentItem = userList[position]
+        holder.itemView.tv.text = currentItem.id.toString()
+        holder.itemView.firstname.text = currentItem.firstName
+        holder.itemView.lastname.text = currentItem.lastName
+        holder.itemView.age.text = currentItem.age.toString()
+    }
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+
+    }
+}
--- app/src/main/java/in/gsrathoreniks/room/fragments/list/ListFragment.kt
+++ app/src/main/java/in/gsrathoreniks/room/fragments/list/ListFragment.kt
@@ -1,16 +1,23 @@
 package `in`.gsrathoreniks.room.fragments.list
 
 import `in`.gsrathoreniks.room.R
+import `in`.gsrathoreniks.room.data.UserViewModel
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,19 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_list, container, false)
+
+//        RecyclerView
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+//        userviewmodel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer {user->
+            adapter.setData(user)
+        })
+
 
         view.findViewById<FloatingActionButton>(R.id.fabAdd).setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
