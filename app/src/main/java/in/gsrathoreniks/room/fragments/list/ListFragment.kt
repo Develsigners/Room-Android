0f2a15e04c8eee3c87284b177817c9549ef0f6a8
gsrathoreniks <niks.gajsa@gmail.com>
2020-10-29 16:21:23 +0530

    Insert into DB : done

--- app/src/main/java/in/gsrathoreniks/room/fragments/list/ListFragment.kt
+++ app/src/main/java/in/gsrathoreniks/room/fragments/list/ListFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ListFragment : Fragment() {
@@ -15,7 +17,13 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.fabAdd).setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
     }
 
 }
